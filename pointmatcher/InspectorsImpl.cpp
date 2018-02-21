dcc779b302426e56098513679caec4d268b259a8
David Landry <davidlandry93@gmail.com>
2018-02-21 13:44:57 -0500

    Fixed InspectorsImpl for the new InvalidMatches mechanic

--- pointmatcher/InspectorsImpl.cpp
+++ pointmatcher/InspectorsImpl.cpp
@@ -348,37 +348,33 @@ void InspectorsImpl<T>::AbstractVTKInspector::dumpDataLinks(
 	}
 
 	stream << "CELL_DATA " << matchCount << "\n";
-	dumpScalar("outlier", featureOutlierWeights, readingPtCount, knn, stream);
-	dumpScalar("distance_squared", matches.dists, readingPtCount, knn, stream);
 
-	//stream << "LOOKUP_TABLE alphaOutlier 2\n";
-	//stream << "1 0 0 0.5\n";
-	//stream << "0 1 0 1\n";
-
-}
-
-template<typename T>
-void InspectorsImpl<T>::AbstractVTKInspector::dumpScalar(
-	const std::string name,
-	const Matrix& data,
-	const int readingPtCount,
-	const int knn,
-	std::ostream& stream)
-{
-
-	stream << "SCALARS " << name << " float 1\n";
+	stream << "SCALARS outliers float 1\n";
 	stream << "LOOKUP_TABLE default\n";
 	for (int k = 0; k < knn; k++) // knn
 	{
-		for (int i = 0; i < readingPtCount; ++i) //nb pts
-		{
-			const auto id = matches.ids(k, i);
-			if (id != Matches::InvalidId){
-				stream << featureOutlierWeights(k, i) << "\n";
-			}
-		}
+      for (int i = 0; i < readingPtCount; ++i) //nb pts
+      {
+          const auto id = matches.ids(k, i);
+          if (id != Matches::InvalidId){
+              stream << featureOutlierWeights(k, i) << "\n";
+          }
+      }
 	}
 
+
+	stream << "SCALARS distance_squared float 1\n";
+	stream << "LOOKUP_TABLE default\n";
+	for (int k = 0; k < knn; k++) // knn
+	{
+      for (int i = 0; i < readingPtCount; ++i) //nb pts
+      {
+          const auto id = matches.ids(k, i);
+          if (id != Matches::InvalidId){
+              stream << matches.dists(k, i) << "\n";
+          }
+      }
+	}
 }
 
 template<typename T>
